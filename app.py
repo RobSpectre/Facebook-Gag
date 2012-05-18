f158b584f1ece048a5af5d5cb665a0a5f3c03443
Rob Spectre <rob.spectre@gmail.com>
2012-05-18 13:03:18 -0400

    First draft.

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ app.config.from_pyfile('local_settings.py')
 @app.route('/voice', methods=['GET', 'POST'])
 def voice():
     response = twiml.Response()
-    response.say("Congratulations! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    response.say("Hey there.  It's time for the Facebook IPO.")
+    response.pause()
+    response.say("Go outside.")
     return str(response)
 
 
@@ -30,39 +31,8 @@ def voice():
 @app.route('/sms', methods=['GET', 'POST'])
 def sms():
     response = twiml.Response()
-    response.sms("Congratulation! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    response.sms("Go outside.")
     return str(response)
-
-
-# Twilio Client demo template
-@app.route('/client')
-def client():
-    configuration_error = None
-    for key in ['TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'TWILIO_APP_SID',
-            'TWILIO_CALLER_ID']:
-        if not app.config[key]:
-            configuration_error = "Missing from local_settings.py: " \
-                    "%s" % key
-            token = None
-    if not configuration_error:
-        capability = TwilioCapability(app.config['TWILIO_ACCOUNT_SID'],
-            app.config['TWILIO_AUTH_TOKEN'])
-        capability.allow_client_incoming("joey_ramone")
-        capability.allow_client_outgoing(app.config['TWILIO_APP_SID'])
-        token = capability.generate()
-    return render_template('client.html', token=token,
-            configuration_error=configuration_error)
-
-
-# Installation success page
-@app.route('/')
-def index():
-    params = {
-        'voice_request_url': url_for('.voice', _external=True),
-        'sms_request_url': url_for('.sms', _external=True),
-        'client_url': url_for('.client', _external=True)}
-    return render_template('index.html', params=params)
 
 
 # Handles SIGTERM so that we don't get an error when Heroku wants or needs to
